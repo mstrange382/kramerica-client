a5278766f071450bf69cabd017db7850cfee3bbc
Mitch Strange <mstrange382@gmail.com>
2021-11-14 18:00:07 -0600

    envrionment update

--- src/helpers/environment.jsx
+++ src/helpers/environment.jsx
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
   case "localhost" || "127.0.0.1":
     APIURL = "http://localhost:3000";
     break;
-  case "kra-merica-client.herokuapp.com":
+  case "kra-merica.herokuapp.com":
     APIURL = "https://kra-merica-portal-server.herokuapp.com";
 }
 export default APIURL;
